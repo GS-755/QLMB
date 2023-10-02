0527b74e16629d3d64222833c7f2cb9e46d483f9
Nguyễn Minh Trí <21dh114236@st.huflit.edu.vn>
2023-10-02 17:01:30 +0700

    Remove height calculator feature

--- Controllers/skillissueController.cs
+++ Controllers/skillissueController.cs
@@ -6,14 +6,5 @@ namespace QLMB.Controllers
     {
         // GET: SkillIssue
         public ActionResult Index() => View();
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Index(string height)
-        {
-            ViewBag.DisplayHeight = height;
-
-            return View();
-        }
     }
 }
