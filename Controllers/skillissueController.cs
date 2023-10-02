cd7cce8eced5ea0bc01d1a2ad7b249eb01a659c4
Nguyễn Minh Trí <21dh114236@st.huflit.edu.vn>
2023-10-02 17:00:26 +0700

    Add height calculator

--- Controllers/skillissueController.cs
+++ Controllers/skillissueController.cs
@@ -6,5 +6,14 @@ namespace QLMB.Controllers
     {
         // GET: SkillIssue
         public ActionResult Index() => View();
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(string height)
+        {
+            ViewBag.DisplayHeight = height;
+
+            return View();
+        }
     }
 }
