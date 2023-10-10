a8f6ef6372c6ea3fe78b391e8dd8abf6bc201f25
Nguyễn Minh Trí <21dh114236@st.huflit.edu.vn>
2023-10-11 03:16:29 +0700

    Complete basis of PropertyController.cs

--- Controllers/Test/PropertyController.cs
+++ Controllers/Test/PropertyController.cs
@@ -1,4 +1,5 @@
-﻿using System.Data.Entity;
+﻿using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Web.Mvc;
@@ -13,32 +14,53 @@ namespace QLMB.Controllers.Test
         // GET: Property
         public ActionResult Index()
         {
-            var matBangs = db.MatBangs.Include(m => m.TinhTrang);
-            return View(matBangs.ToList());
-        }
+            try
+            {
+                var matBangs = db.MatBangs.Include(m => m.TinhTrang);
 
+                return View(matBangs.ToList());
+            }
+            catch 
+            {
+                return RedirectToAction("Index", "SkillIssue");
+            }
+        }
         // GET: Property/Details/5
         public ActionResult Details(string id)
         {
-            if (id == null)
+            try
             {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                if (id == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+                MatBang matBang = db.MatBangs.Find(id);
+                if (matBang == null)
+                {
+                    return HttpNotFound();
+                }
+                return View(matBang);
             }
-            MatBang matBang = db.MatBangs.Find(id);
-            if (matBang == null)
+            catch
             {
-                return HttpNotFound();
+                return RedirectToAction("Index", "SkillIssue");
             }
-            return View(matBang);
         }
-
         // GET: Property/Create
         public ActionResult Create()
         {
-            ViewBag.MATT = new SelectList(db.TinhTrangs, "MATT", "TenTT");
-            return View();
-        }
+            try
+            {
+                List<TinhTrang> dsTinhTrang = db.TinhTrangs.Where(k => k.MATT >= 7).ToList();
+                ViewBag.MATT = new SelectList(dsTinhTrang, "MaTT", "TenTT");
 
+                return View();
+            }
+            catch
+            {
+                return RedirectToAction("Index", "SkillIssue");
+            }
+        }
         // POST: Property/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
@@ -46,33 +68,47 @@ namespace QLMB.Controllers.Test
         [ValidateAntiForgeryToken]
         public ActionResult Create([Bind(Include = "MaMB,Lau,DienTich,Khu,TienThue,MATT")] MatBang matBang)
         {
-            if (ModelState.IsValid)
+            try
             {
-                db.MatBangs.Add(matBang);
-                db.SaveChanges();
-                return RedirectToAction("Index");
+                if (ModelState.IsValid)
+                {
+                    db.MatBangs.Add(matBang);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                ViewBag.MATT = new SelectList(db.TinhTrangs, "MATT", "TenTT", matBang.MATT);
+
+                return View(matBang);
             }
-
-            ViewBag.MATT = new SelectList(db.TinhTrangs, "MATT", "TenTT", matBang.MATT);
-            return View(matBang);
+            catch
+            {
+                return RedirectToAction("Index", "SkillIssue");
+            }
         }
-
         // GET: Property/Edit/5
         public ActionResult Edit(string id)
         {
-            if (id == null)
+            try
             {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            MatBang matBang = db.MatBangs.Find(id);
-            if (matBang == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.MATT = new SelectList(db.TinhTrangs, "MATT", "TenTT", matBang.MATT);
-            return View(matBang);
-        }
+                if (id == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+                MatBang matBang = db.MatBangs.Find(id);
+                if (matBang == null)
+                {
+                    return HttpNotFound();
+                }
+                List<TinhTrang> dsTinhTrang = db.TinhTrangs.Where(k => k.MATT >= 7).ToList();
+                ViewBag.MATT = new SelectList(dsTinhTrang, "MaTT", "TenTT", matBang.MATT);
 
+                return View(matBang);
+            }
+            catch
+            {
+                return RedirectToAction("Index", "SkillIssue");
+            }
+        }
         // POST: Property/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
@@ -80,42 +116,65 @@ namespace QLMB.Controllers.Test
         [ValidateAntiForgeryToken]
         public ActionResult Edit([Bind(Include = "MaMB,Lau,DienTich,Khu,TienThue,MATT")] MatBang matBang)
         {
-            if (ModelState.IsValid)
+            try
             {
-                db.Entry(matBang).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            ViewBag.MATT = new SelectList(db.TinhTrangs, "MATT", "TenTT", matBang.MATT);
-            return View(matBang);
-        }
+                if (ModelState.IsValid)
+                {
+                    db.Entry(matBang).State = EntityState.Modified;
+                    db.SaveChanges();
 
+                    return RedirectToAction("Index");
+                }
+                List<TinhTrang> dsTinhTrang = db.TinhTrangs.Where(k => k.MATT >= 7).ToList();
+                ViewBag.MATT = new SelectList(dsTinhTrang, "MaTT", "TenTT", matBang.MATT);
+
+                return View(matBang);
+            }
+            catch
+            {
+                return RedirectToAction("Index", "SkillIssue");
+            }
+        }
         // GET: Property/Delete/5
         public ActionResult Delete(string id)
         {
-            if (id == null)
+            try
             {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            MatBang matBang = db.MatBangs.Find(id);
-            if (matBang == null)
-            {
-                return HttpNotFound();
-            }
-            return View(matBang);
-        }
+                if (id == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+                MatBang matBang = db.MatBangs.Find(id);
+                if (matBang == null)
+                {
+                    return HttpNotFound();
+                }
 
+                return View(matBang);
+            }
+            catch
+            {
+                return RedirectToAction("Index", "SkillIssue");
+            }
+        }
         // POST: Property/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(string id)
         {
-            MatBang matBang = db.MatBangs.Find(id);
-            db.MatBangs.Remove(matBang);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
+            try
+            {
+                MatBang matBang = db.MatBangs.Find(id);
+                db.MatBangs.Remove(matBang);
+                db.SaveChanges();
 
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return RedirectToAction("Index", "SkillIssue");
+            }
+        }
         protected override void Dispose(bool disposing)
         {
             if (disposing)
