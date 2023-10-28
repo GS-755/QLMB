6bbc718ee23934edf81b1f30aabf2c309cc0fa7e
Nguyễn Minh Trí <21dh114236@st.huflit.edu.vn>
2023-10-28 18:07:54 +0700

    Add RandomID.cs class

---
+++ Models/Process/RandomID.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+namespace QLMB.Models
+{
+    public class RandomID
+    {
+        private static Random random = new Random();
+        private static readonly string characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+        public static string Get(int size = 6)
+        {
+            StringBuilder builder = new StringBuilder(size);
+            for (int i = 0; i < size; i++)
+            {
+                int randomIndex = random.Next(characters.Length);
+                char randomChar = characters[randomIndex];
+                builder.Append(randomChar);
+            }
+
+            return builder.ToString();
+        }
+    }
+}
